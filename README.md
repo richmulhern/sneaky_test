2ecb8ef6e301d0333bb2bfa62de4c3a60b78bc4d
Richard Mulhern <rmulhern@ariasystems.com>
2021-08-21 21:46:02 -0400

    Added test and solution.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # sneaky_test
 Interview Code Challenge Solution
+
+### Problem
+
+Assume there is a room of 16 students in a classroom in a grid from [0,0] to [3,3]. The students are attempting to pass notes without getting caught. Some students are sneakier than others and each student is given a percentage value of how sneaky they are. Calculate the path from one student to another that has the least likeliest chance of being caught.
+
+### Rules
+- You start with 100% chance of getting caught.
+- Notes can only be passed in a positive direction on the grid. A note starting at [1,2] can only go to a student whose x position is greater than 1 and y position is greater than 2.
+- Notes need to be passed from student to student along the way.
+- Notes cannot be passed diagonally.
+- Notes passed on the x axis remove at most 10 from the current chance of getting caught (e.g. 0,0 to 1,0 would go from 100 to 90 then 1,0 to 2,0 would be 80)
+- Notes passed on the y axis remove at most 50% of the current chance of getting caught from the value (e.g. 0,0 to 0,1 would change your chance of getting caught from 100 to 50, but from 0,1 to 0,2 it would go from 50 to 25)
+- The sneakiness of the current student with the note is a percentage that applies to the points that will be lost with the move. If a student has a sneaky rating of 50 and moves horizontally the chance of getting caught only goes down 5%. If the same student moves across the 7 access they now only 50% of the 50% to be reduced (25%).
---
+++ sneaky.js
@@ -0,0 +1,56 @@
+/**
+ * This is not part of the solution but to test it I made up this function to
+ * randomly make up "sneakiness grids" to make sure I'm not always getting the
+ * same path.
+ */
+function generateSneakyGrid(x, y, i, j)
+{
+    let sneakyGrid = [];
+
+    for (let a = x; a <= i; a++) {
+        sneakyGrid[a] = [];
+        for(let b = y; b <= j; b++) {
+            sneakyGrid[a][b] = Math.random();
+        }
+    }
+
+    return sneakyGrid;
+}
+
+var sneakyGrid = generateSneakyGrid(0, 0, 3, 3);
+
+function passNote(x, y, i, j, chance = 100)
+{
+    // if you're note has arrived at it's destination send back the final chance amount
+    if (x == i && y == j) {
+        return {'path': [[x,y]], 'chance': chance};
+    }
+
+    // initialize x and y data, this way you have something to compare against
+    // when comparing a node just before the final node and the final node
+    let xData = {'path': [], 'chance': 100};
+    let yData = {'path': [], 'chance': 100};
+    let sneaky = sneakyGrid[x][y];
+
+    // if there is space to move across the x axis check that path
+    if (x < i) {
+        xData = passNote(x+1, y, i, j, chance - (10 * sneaky));
+    }
+
+    // if there is space to move across the y axis check that path
+    if (y < j) {
+        yData = passNote(x, y +1, i, j, chance - ((chance/2) * sneaky));
+    }
+
+    // compare which path is best
+    if (xData.chance < yData.chance) {
+        bestData = xData;
+    } else {
+        bestData = yData;
+    }
+
+    // return the best path from this point and it's chance to the previous student
+    return {'path': [].concat([[x,y]], bestData.path), 'chance': bestData.chance}
+}
+
+console.log(passNote(0, 0, 3, 3));
